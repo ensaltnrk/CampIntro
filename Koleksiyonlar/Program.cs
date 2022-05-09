8f3850d9b674a29c35cdb354ec4017ce9f3c38fe
vittlys up <vittlys@hotmail.com>
2022-05-09 22:38:01 +0300

    C# Camp2

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = new string[] { "Engin", "Murat", "Enes", "Halil" };
+            Console.WriteLine(name[0]);
+            Console.WriteLine(name[1]);
+            Console.WriteLine(name[2]);
+            Console.WriteLine(name[3]);
+            Console.WriteLine("..................");
+
+            List<string> name2 = new List<string> { "Engin", "Murat", "Enes", "Halil" };
+            Console.WriteLine(name[0]);
+            Console.WriteLine(name[1]);
+            Console.WriteLine(name[2]);
+            Console.WriteLine(name[3]);
+
+            name2.Add("İlker");
+            Console.WriteLine(name2[4]);
+            Console.WriteLine(name2[0]);
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product//Ürün
+    {
+        public int  Id { get; set; }//Ürün idsi.
+
+        public int CategoryId { get; set; }//Kategori idsi.
+
+        public string ProductName { get; set; }//Ürünün ismi.
+
+        public double UnitPrice { get; set; }//Ürünün birim fiyatı.
+
+        public int UnitInStock { get; set; }//Ürün stoğu.
+
+    }
+}
